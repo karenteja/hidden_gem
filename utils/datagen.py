c716c470e2a2753e3e397ac49b25d400c82f214d
pzaytsev <zaytsev.pavel.a@gmail.com>
2019-12-01 17:34:22 -0800

    ETL work

--- utils/datagen.py
+++ utils/datagen.py
@@ -1,10 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python
 
+import argparse
 from argparse import ArgumentParser
 import requests as r
-from requests.exceptions import RequestException
 import json
-import urllib
 import sys
 
 """
@@ -15,7 +14,7 @@ It optionally saves the extracted data too.
 """
 
 DEFAULT_DATA_DIRS = {
-	'bars': '../data/bars.json'
+    'bars': '../data/bars.json'
 }
 
 DEFAULT_HOST = 'https://api.yelp.com'
@@ -26,70 +25,72 @@ DEFAULT_LOCATION = 'San Francisco, CA'
 
 
 def search(host=DEFAULT_HOST, path=DEFAULT_PATH, keys=None, url_params=None):
-	if not keys:
-		raise ValueError('Keys are not defined.')
-	api_key = keys['api_key']
-	headers = {'Authorization': f'Bearer {api_key}'}
-	url_params = url_params or {}
-	url = f'{host}/{path}'
-	try:
-		response = r.request('GET', url, headers=headers, params=url_params)
-		response.raise_for_status()
-		return response.json()
-	except Exception as e:
-		print(e)
-		sys.exit(1)
+    if not keys:
+        raise ValueError('Keys are not defined.')
+    api_key = keys['api_key']
+    headers = {'Authorization': f'Bearer {api_key}'}
+    url_params = url_params or {}
+    url = f'{host}/{path}'
+    try:
+        response = r.request('GET', url, headers=headers, params=url_params)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(e)
+        sys.exit(1)
 
 
 def create_url_params_list(location, term, limit):
-	max_results_per_request = 50
-	if limit <= max_results_per_request:
-		raise ValueError("One url params is enough.")
-	else:
-		r = range(0, limit)
-		splits = [r[i:i + max_results_per_request] for i in range(0, len(r), max_results_per_request)]
-		extra_params = [{'term': term.replace(' ', '+'), 'location': location.replace(' ', '+'), 'limit': len(request), 'offset': request[0] + 1} for request in splits]
-		return extra_params
+    max_results_per_request = 50
+    if limit <= max_results_per_request:
+        raise ValueError("One url params is enough.")
+    else:
+        r = range(0, limit)
+        splits = [r[i:i + max_results_per_request] for i in range(0, len(r), max_results_per_request)]
+        extra_params = [{'term': term.replace(' ', '+'), 'location': location.replace(' ', '+'), 'limit': len(request),
+                         'offset': request[0] + 1} for request in splits]
+        return extra_params
 
 
 def search_in_location(location, term, limit):
-	if location is None or term is None or limit is None:
-		raise ValueError("Location, term and limit should be defined.")
+    if location is None or term is None or limit is None:
+        raise ValueError("Location, term and limit should be defined.")
 
-	if limit > 50:
-		url_params_list = create_url_params_list(location, term, limit)
-		return [search(keys=populate_keys(), url_params=url_params) for url_params in url_params_list]
+    if limit > 50:
+        url_params_list = create_url_params_list(location, term, limit)
+        return [search(keys=populate_keys(), url_params=url_params) for url_params in url_params_list]
 
-	else:
-		url_params = {
-		'term': term.replace(' ', '+'),
-		'location': location.replace(' ', '+'),
-		'limit': limit
-		}
-		return search(keys=populate_keys(), url_params=url_params)
-	
+    else:
+        url_params = {
+            'term': term.replace(' ', '+'),
+            'location': location.replace(' ', '+'),
+            'limit': limit
+        }
+        return search(keys=populate_keys(), url_params=url_params)
+
 
 def save_locally(data):
-	if data is None:
-		raise ValueError('Data should be present to save it.')
+    if data is None:
+        raise ValueError('Data should be present to save it.')
 
-	with open(DEFAULT_DATA_DIRS['bars'], 'w') as f:
-		json.dump(data, f, indent=4, sort_keys=True)
-	print(f'Saved.')
+    with open(DEFAULT_DATA_DIRS['bars'], 'w') as f:
+        json.dump(data, f, indent=4, sort_keys=True)
+    print(f'Saved.')
+
 
 def populate_keys():
-	with open('key', 'r') as f:
-		client_id = f.readline().split(',')[1].strip()
-		api_key = f.readline().split(',')[1].strip()
-		keys = {}
-		keys['client_id'] = client_id
-		keys['api_key'] = api_key
-		return keys
+    with open('key', 'r') as f:
+        client_id = f.readline().split(',')[1].strip()
+        api_key = f.readline().split(',')[1].strip()
+        keys = {}
+        keys['client_id'] = client_id
+        keys['api_key'] = api_key
+        return keys
 
 
 def str2bool(v):
     if isinstance(v, bool):
-       return v
+        return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
@@ -99,24 +100,28 @@ def str2bool(v):
 
 
 def main():
-	
-	parser = ArgumentParser(description="Get some bar/restaurant data.")
-	parser.add_argument('-q', '--term', dest='term', default=DEFAULT_TERM, type=str, help=f'Search term, e.g. {DEFAULT_TERM}.')
-	parser.add_argument('-l', '--location', dest='loc', default=DEFAULT_LOCATION, type=str, help=f'Search location, e.g. {DEFAULT_LOCATION}.')
-	parser.add_argument('-li', '--limit', dest='lim', default=DEFAULT_SEARCH_LIMIT, type=int, help=f'Search limit, e.g. 3')
-	parser.add_argument('-s', '--save', dest='save', type=str2bool, nargs='?', const=True, default=False, help='Save to a local file. If false just prints data to stdout.')
-	params = parser.parse_args()
-	data = search_in_location(params.loc, params.term, params.lim)
+    parser = ArgumentParser(description="Get some bar/restaurant data.")
+    parser.add_argument('-q', '--term', dest='term', default=DEFAULT_TERM, type=str,
+                        help=f'Search term, e.g. {DEFAULT_TERM}.')
+    parser.add_argument('-l', '--location', dest='loc', default=DEFAULT_LOCATION, type=str,
+                        help=f'Search location, e.g. {DEFAULT_LOCATION}.')
+    parser.add_argument('-li', '--limit', dest='lim', default=DEFAULT_SEARCH_LIMIT, type=int,
+                        help=f'Search limit, e.g. 3')
+    parser.add_argument('-s', '--save', dest='save', type=str2bool, nargs='?', const=True, default=False,
+                        help='Save to a local file. If false just prints data to stdout.')
+    params = parser.parse_args()
+    data = search_in_location(params.loc, params.term, params.lim)
 
-	if params.lim > 900:
-		raise ValueError("Can't get more than 900 locations.")
-	if data is not None:
-		if params.save:
-			print('Saving the data')
-			save_locally(data)
-		else:
-			print('Here is a piece of your data:')
-			print(data[0]['businesses'][0])
+    if params.lim > 900:
+        raise ValueError("Can't get more than 900 locations.")
+    if data is not None:
+        if params.save:
+            print('Saving the data')
+            save_locally(data)
+        else:
+            print('Here is a piece of your data:')
+            print(data[0]['businesses'][0])
+
 
 if __name__ == '__main__':
     main()
--- utils/etl.py
+++ utils/etl.py
@@ -1,24 +1,156 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python
 
-import sys
-import os
 from os import path
 import json
-
-
+from typing import List, Optional, Dict
+from dataclasses import dataclass, field
+from dataclasses_json import dataclass_json
 
 DEFAULT_PEOPLE_SOURCE = '../data/people.json'
 DEFAULT_BAR_SOURCE = '../data/bars.json'
+DEFAULT_SEED = 0
+DEFAULT_MAX_FAVORITES = 10
+DEFAULT_MAX_SAVED_FOR_LATER = 20
+DEFAULT_MAX_EXPLORED = 30
+
+
+@dataclass
+@dataclass_json
+class USLocation:
+    """A data class that represents location in the US."""
+    address1: str = field(default="n/a")
+    address2: Optional[str] = field(default=None)
+    address3: Optional[str] = field(default=None)
+    city: str = field(default="n/a")
+    state: str = field(default="n/a")
+    zip: str = field(default="n/a")
+
+
+@dataclass
+@dataclass_json
+class Coordinates:
+    """A data class that represents coordinates."""
+    lat: float = field(default=0)
+    lon: float = field(default=0)
+
+
+@dataclass
+@dataclass_json
+class Bar:
+    """A data class that represents a bar."""
+    categories: List[str] = field(default_factory=list)
+    coordinates: Coordinates = field(default=Coordinates())
+    image_url: str = field(default='n/a')
+    is_closed: bool = field(default=True)
+    location: USLocation = field(default=USLocation())
+    name: str = field(default='n/a')  #
+    id: str = field(default='n/a')
+    phone: Optional[str] = field(default=None)
+    price: Optional[str] = field(default=None)
 
 
 def check_data_present(data_source_path, bar_source_path):
-	if path.exists(data_source_path) and path.exists(bar_source_path):
-		return
-	raise IOError('Data sources should exist.')
+    if path.exists(data_source_path) and path.exists(bar_source_path):
+        return
+    raise IOError('Data sources should exist.')
+
+
+def parse_categories(business: Dict) -> List[str]:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    return [x['alias'] for x in business['categories']]
+
+
+def parse_coords(business: Dict) -> Coordinates:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    lat = business['coordinates']['latitude']
+    lon = business['coordinates']['longitude']
+    return Coordinates(lat, lon)
+
+
+def parse_image_url(business: Dict) -> str:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    return business['image_url']
+
+
+def parse_is_closed(business: Dict) -> bool:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    return business['is_closed']
+
+
+def parse_location(business: Dict) -> USLocation:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    address1 = business['location']['address1']
+    address2 = business['location']['address2']
+    address3 = business['location']['address3']
+    city = business['location']['city']
+    state = business['location']['state']
+    zip = business['location']['zip_code']
+    return USLocation(address1, address2, address3, city, state, zip)
+
+
+def parse_name(business: Dict) -> str:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    return business['name']
+
+
+def parse_id(business: Dict) -> str:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    return business['id']
+
+
+def parse_phone(business: Dict) -> Optional[str]:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    return business.get('phone', None)
+
+
+def parse_price(business: Dict) -> Optional[str]:
+    if business is None:
+        raise ValueError("Business should be defined.")
+    return business.get('price', None)
+
+
+def load_bar_data(bar_source_path) -> List[Bar]:
+    with open(bar_source_path, 'r') as f:
+        json_object = json.load(f)
+        bar_list = []
+        for business_object in json_object:
+            for business in business_object['businesses']:
+                # print(business)
+                price = parse_price(business)
+                phone = parse_phone(business)
+                id = parse_id(business)
+                name = parse_name(business)
+                location = parse_location(business)
+                is_closed = parse_is_closed(business)
+                image_url = parse_image_url(business)
+                coords = parse_coords(business)
+                categories = parse_categories(business)
+                bar_list.append(Bar(categories, coords, image_url, is_closed, location, name, id, phone, price))
+        return bar_list
+
+
+def create_favorites():
+    pass
+
+
+def create_explored():
+    pass
+
+
+def create_saved_for_later():
+    pass
 
 
 def main():
-	print("LOL")
+
 
 if __name__ == '__main__':
     main()
