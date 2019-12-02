752a9d4f0740e484ecd8de02108e7e501daa7ce6
pzaytsev <zaytsev.pavel.a@gmail.com>
2019-12-01 19:37:15 -0800

    Data new

--- utils/etl.py
+++ utils/etl.py
@@ -2,19 +2,20 @@
 
 from os import path
 import json
-from typing import List, Optional, Dict
-from dataclasses import dataclass, field
+from typing import List, Optional, Dict, Set, Tuple, Any
+from dataclasses import dataclass, field, replace
 from dataclasses_json import dataclass_json
+from random import sample, seed, randint
 
 DEFAULT_PEOPLE_SOURCE = '../data/people.json'
 DEFAULT_BAR_SOURCE = '../data/bars.json'
-DEFAULT_SEED = 0
+DEFAULT_NEW_PEOPLE_DEST = '../data/data_new.json'
 DEFAULT_MAX_FAVORITES = 10
 DEFAULT_MAX_SAVED_FOR_LATER = 20
 DEFAULT_MAX_EXPLORED = 30
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 @dataclass_json
 class USLocation:
     """A data class that represents location in the US."""
@@ -26,7 +27,7 @@ class USLocation:
     zip: str = field(default="n/a")
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 @dataclass_json
 class Coordinates:
     """A data class that represents coordinates."""
@@ -34,11 +35,11 @@ class Coordinates:
     lon: float = field(default=0)
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 @dataclass_json
 class Bar:
     """A data class that represents a bar."""
-    categories: List[str] = field(default_factory=list)
+    categories: Tuple[str] = field(default=())
     coordinates: Coordinates = field(default=Coordinates())
     image_url: str = field(default='n/a')
     is_closed: bool = field(default=True)
@@ -49,16 +50,29 @@ class Bar:
     price: Optional[str] = field(default=None)
 
 
-def check_data_present(data_source_path, bar_source_path):
-    if path.exists(data_source_path) and path.exists(bar_source_path):
+@dataclass
+@dataclass_json
+class User:
+    """A data class that represents a user."""
+    id: int = field(default=0)
+    name: str = field(default='n/a')
+    username: str = field(default='n/a')
+    email: str = field(default='n/a')
+    password: str = field(default='n/a')
+    img: str = field(default='n/a')
+    bars: Dict = field(default_factory=lambda: {})
+
+
+def check_data_present(user_source_path, bar_source_path):
+    if path.exists(user_source_path) and path.exists(bar_source_path):
         return
     raise IOError('Data sources should exist.')
 
 
-def parse_categories(business: Dict) -> List[str]:
+def parse_categories(business: Dict) -> Tuple[Any]:
     if business is None:
         raise ValueError("Business should be defined.")
-    return [x['alias'] for x in business['categories']]
+    return tuple([x['alias'] for x in business['categories']])
 
 
 def parse_coords(business: Dict) -> Coordinates:
@@ -137,19 +151,50 @@ def load_bar_data(bar_source_path) -> List[Bar]:
         return bar_list
 
 
-def create_favorites():
-    pass
+def load_user_data(user_source_path) -> List[User]:
+    with open(user_source_path, 'r') as f:
+        users = json.load(f)
+        user_list = []
+        for user in users:
+            id = user['id']
+            name = user['name']
+            username = user['username']
+            email = user['email']
+            password = user['password']
+            img = user['img']
+            user_list.append(User(id, name, username, email, password, img))
+        return user_list
 
 
-def create_explored():
-    pass
+def create_user_bar_lists(bars: List[Bar], s: int) -> Dict:
+    seed(s)
+    selected = {}
+    all_user_bars = set(sample(bars, DEFAULT_MAX_SAVED_FOR_LATER + DEFAULT_MAX_EXPLORED))
+    selected['explored'] = sample(all_user_bars, randint(0, DEFAULT_MAX_EXPLORED))
+    saved_for_later = all_user_bars - set(selected['explored'])
+    selected['saved'] = sample(saved_for_later, min(randint(0, DEFAULT_MAX_SAVED_FOR_LATER), len(saved_for_later)))
+    selected['favorite'] = sample(selected['explored'],
+                                  min(randint(0, DEFAULT_MAX_FAVORITES), len(selected['explored'])))
+    return selected
 
 
-def create_saved_for_later():
-    pass
+def set_user_bar_data(user: User, bars: Dict) -> User:
+    return replace(user, bars=bars)
+
+
+def augment_user_data(users: List[User], bars: List[Bar]) -> List[User]:
+    return [set_user_bar_data(users[i], create_user_bar_lists(bars, i)) for i in range(0, len(users))]
+
+
+def write_new_data(users: List[User], dest_name: str):
+    with open(dest_name, 'w', encoding='utf-8') as f:
+        f.write(json.dumps([json.loads(user.to_json()) for user in users], indent=4, ensure_ascii=False))
 
 
 def main():
+    check_data_present(DEFAULT_PEOPLE_SOURCE, DEFAULT_BAR_SOURCE)
+    users = augment_user_data(load_user_data(DEFAULT_PEOPLE_SOURCE), load_bar_data(DEFAULT_BAR_SOURCE))
+    write_new_data(users, DEFAULT_NEW_PEOPLE_DEST)
 
 
 if __name__ == '__main__':
